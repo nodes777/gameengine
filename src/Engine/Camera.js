cedf78e7bf1381e4bcb707705509018f03642e73
nodes777 <nodes777@gmail.com>
2017-05-28 10:49:07 -0400

    adds jsdoc to Camera and Renderable

--- src/Engine/Camera.js
+++ src/Engine/Camera.js
@@ -9,22 +9,23 @@
 
 "use strict";
 
-/** wcCenter: is a vec2
-* wcWidth: is the width of the user defined WC
-*      Height of the user defined WC is implicitly defined by the viewport aspect ratio
-*      Please refer to the following
-* viewportRect: an array of 4 elements
-*      [0] [1]: (x,y) position of lower left corner on the canvas (in pixel)
-*      [2]: width of viewport
-*      [3]: height of viewport
-*
-*  wcHeight = wcWidth * viewport[3]/viewport[2]
+/**
+* @constructor
+* @param {vec2} wcCenter - Center of the world coordinates
+* @param {number} wcWidth -Width of the user defined WC. Height of the user defined WC is implicitly defined by the viewport aspect ratio
+* @param {Object[]} viewportRect - an array of 4 elements
+* @param {number} viewportRect[].x - x position of lower left corner on the canvas (in pixels)
+* @param {number} viewportRect[].y - (x,y) position of lower left corner on the canvas (in pixels)
+* @param {number} viewportRect[].width - width of viewport
+* @param {number} viewportRect[].height - height of viewport
 */
 function Camera(wcCenter, wcWidth, viewportArray) {
 	/** World Coordinates and viewport position and size */
 	this.mWCCenter = wcCenter;
-	/** the height of the WC is always computed from the width
-	// this guarantees a matching aspect ratio between WC and viewport*/
+	/** The height of the WC is always computed from the width.
+	* This guarantees a matching aspect ratio between WC and viewport
+	* wcHeight = wcWidth * viewport[3]/viewport[2]
+	*/
 	this.mWCWidth = wcWidth;
 	// [x,y,width,height]
 	// x and y are of lower left corner
@@ -64,11 +65,13 @@ Camera.prototype.setupViewProjection = function () {
 	var gl = gEngine.Core.getGL();
 	// Step A: configure viewport
 	// Step A1: Set up the viewport: area on canvas to be drawn
-	/** @function - specifies the affine transformation of x and y from normalized device coordinates to window coordinates.
-	*	@param {number} x
- 	* 	@param {number} y
- 	* 	@param {number} width
- 	* 	@param {number} height
+	/**
+	* Specifies the affine transformation of x and y from normalized device coordinates to window coordinates.
+	* @function
+	* @param {number} x
+ 	* @param {number} y
+ 	* @param {number} width
+ 	* @param {number} height
  	*/
 	gl.viewport(this.mViewport[0], this.mViewport[1], this.mViewport[2], this.mViewport[3]);
 	// Step A2: set up the corresponding scissor area to limit clear area
@@ -83,11 +86,13 @@ Camera.prototype.setupViewProjection = function () {
 	// Step B: define the View-Projection matrix
 
 	// Step B1:
-	/** @function - define the view matrix, a given func from webGL lib
-	*	@param {object} viewMatrix - assign the properties to this viewMatrix
- 	* 	@param {array} eye - World Coord center, position of the viewer
- 	* 	@param {number} center - Look At Position, point the viewer is looking at
- 	* 	@param {number} up - Orientation, vec3 pointing up
+	/**
+	* Define the view matrix, a given func from webGL lib
+	* @function lookAt
+	* @param {object} viewMatrix - assign the properties to this viewMatrix
+ 	* @param {array} eye - World Coord center, position of the viewer
+ 	* @param {number} center - Look At Position, point the viewer is looking at
+ 	* @param {number} up - Orientation, vec3 pointing up
  	*/
 	mat4.lookAt(this.mViewMatrix,
 		[this.mWCCenter[0], this.mWCCenter[1], 10], // WC center
@@ -99,15 +104,17 @@ Camera.prototype.setupViewProjection = function () {
 	// WCHeight = WCWidth * viewportHeight / viewportWidth, why????
 	var halfWCHeight = halfWCWidth * this.mViewport[3]/this.mViewport[2];
 
-	/** @function - Generates a orthogonal projection matrix with the given bounds, a given func from webGL lib
-	 * @param {mat4} defines the dimesion of the WC, out mat4 frustum matrix will be written into
-	 * @param {number} distant to left of WC, left Left bound of the frustum
-	 * @param {number} distant to right of WC, right Right bound of the frustum
-	 * @param {number} distant to bottom of WC, bottom Bottom bound of the frustum
-	 * @param {number} distant to top of WC, top Top bound of the frustum
-	 * @param {number} z-distant to near plane, near Near bound of the frustum
-	 * @param {number} z-distant to far plane, far Far bound of the frustum
-	 * @returns {mat4} out
+	/**
+	* Generates a orthogonal projection matrix with the given bounds, a given func from webGL lib
+	* @function ortho
+	* @param {mat4} defines the dimesion of the WC, out mat4 frustum matrix will be written into
+	* @param {number} distant to left of WC, left Left bound of the frustum
+	* @param {number} distant to right of WC, right Right bound of the frustum
+	* @param {number} distant to bottom of WC, bottom Bottom bound of the frustum
+	* @param {number} distant to top of WC, top Top bound of the frustum
+	* @param {number} z-distant to near plane, near Near bound of the frustum
+	* @param {number} z-distant to far plane, far Far bound of the frustum
+	* @returns {mat4} out
  	*/
 	mat4.ortho(this.mProjMatrix, //defines the dimesion of the WC
 		-halfWCWidth,      // distant to left of WC
@@ -117,6 +124,12 @@ Camera.prototype.setupViewProjection = function () {
          this.mNearPlane,  // z-distant to near plane
          this.mFarPlane    // z-distant to far plane
 	);
-	/** @function - applies the matrix transforms*/
+	/**
+	* Applies the matrix transforms
+	* @function multiply
+	* @param {mat4} view projection matrix
+	* @param {mat4} projection matrix
+	* @param {mat4} view matrix
+	*/
 	mat4.multiply(this.mVPMatrix, this.mProjMatrix, this.mViewMatrix);
 };
